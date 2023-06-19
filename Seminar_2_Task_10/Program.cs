cca2e3206d2eea7e4721cb0a39cc16b56edaa48c
[Gaia <[dzhayadzoy@gmail.com]>
2023-06-19 11:44:07 +0500

    task_10_solution

---
+++ Seminar_2_Task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Урок 2. Базовые алгоритмы
+
+Задача 10: Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int SecDigit(int number)
+
+{
+    number =  number / 10 % 10;
+    return number;
+}
+
+Console.WriteLine("Input three digit number => ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SecDigit(number);
+Console.WriteLine(result); 
