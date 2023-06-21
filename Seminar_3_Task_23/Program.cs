63cfa3e036d30d5758390c51dcff3a282291bfca
[Gaia <[dzhayadzoy@gmail.com]>
2023-06-21 15:53:20 +0500

    added_task_25

--- Seminar_3_Task_23/Program.cs
+++ Seminar_3_Task_23/Program.cs
@@ -4,16 +4,6 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125 */
 
-
-/*Console.Write("Enter number ");
-int number = Convert.ToInt32(Console.ReadLine());
-
- for (int i = 1; i <= number; i++)
-    {
-        Console.WriteLine($"{Math.Pow(i, 3)}");
-    }
-*/
-
 Console.Write("Enter number ");
 int number = Convert.ToInt32(Console.ReadLine());
 
