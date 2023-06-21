a5f7d3330abff3cb19445e89b36ffa26c990d6d6
[Gaia <[dzhayadzoy@gmail.com]>
2023-06-21 13:47:52 +0500

    task_21_solution

---
+++ Seminar_3_task_21/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+void Print(string text) {
+    Console.WriteLine(text);
+    Console.ForegroundColor = ConsoleColor.Magenta;
+}
+    
+int GetIntNum(){
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+
+}
+
+double MeasureDistance (int x1, int y1, int z1, int x2, int y2, int z2) {
+    double result = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 -y2) + (z1 - z2) * (z1 - z2));
+    return result;
+}
+
+Print("Enter x1, y1, z1, x2, y2, z2 :  " );
+int x1 = GetIntNum();
+int y1 = GetIntNum();
+int z1 = GetIntNum();
+int x2 = GetIntNum();
+int y2 = GetIntNum();
+int z2 = GetIntNum();
+
+double result = MeasureDistance(x1, y1, z1, x2, y2, z2);
+Print ($"Distance between points is {result}"); 
