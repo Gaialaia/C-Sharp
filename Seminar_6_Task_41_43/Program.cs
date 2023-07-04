9d2eaadddc9693db661db943071fc7287a75ae0d
[Gaia <[dzhayadzoy@gmail.com]>
2023-07-04 15:14:50 +0500

    added an example of ideal solution

--- Seminar_6_Task_41_43/Program.cs
+++ Seminar_6_Task_41_43/Program.cs
@@ -6,6 +6,54 @@
 
 
 
+
+
+int Prompt (string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result; 
+}
+
+int [] InputArray (int length) {
+    int [] array = new int [length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt ($"Insert {i+1} element");
+    }
+    return array;
+}
+
+void PrintArray (int [] array) {
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int CountPositiveNumbers (int [] array)
+
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] > 0)
+        count ++;
+    }
+
+    return count;
+
+
+}
+
+int length = Prompt ("Insert element quantity => ");
+int[,] array;
+array = InputArray(length);
+//PrintArray(array);
+Console.WriteLine($"Quantity of numbers < 0 {CountPositiveNumbers(array)}");
+
+
 /*Console.WriteLine("Введите число");
 
 string number = Console.ReadLine();
@@ -27,9 +75,9 @@ for (int i = 0; i < 3;)
 /*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
  значения b1, k1, b2 и k2 задаются пользователем.
-b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Введите k1: ");
+/Console.WriteLine("Введите k1: ");
 var k1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите b1: ");
 var b1 = Convert.ToDouble(Console.ReadLine());
@@ -45,4 +93,4 @@ var y = k1 * x + b1;
 x = Math.Round(x, 3);
 y = Math.Round(y, 3);
  
-Console.WriteLine($"Точка пересечения двух прямых: ({x};{y})");
+Console.WriteLine($"Точка пересечения двух прямых: ({x};{y})"); */
