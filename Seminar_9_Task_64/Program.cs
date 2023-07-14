6ffe1fce3f6ec996092a5c1a744b67d791243759
[Gaia <[dzhayadzoy@gmail.com]>
2023-07-14 14:24:13 +0500

    task_64_solution

---
+++ Seminar_9_Task_64/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа
+ в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+Console.WriteLine("Enter value1 ");
+int value1 = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter value ");
+int value2 = Int32.Parse(Console.ReadLine());
+
+void Print (int value1, int value2) {
+   
+    if (value1 < value2) Print (value1 + 1, value2);
+     Console.WriteLine (value1);
+}
+Print(value1, value2); 
+
+
+
+
+/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
---
+++ Seminar_9_Task_66/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+
+Console.WriteLine("Enter value1 ");
+int value1 = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter value ");
+int value2 = Int32.Parse(Console.ReadLine());
+
+void Print (int value1, int value2) {
+   
+    if (value1 < value2) Print (value1 + 1, value2);
+     Console.WriteLine (value1);
+}
+Print(value1, value2); 
+
